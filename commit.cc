e10d8983c1a65a1a667c4361ecb8e3044b73d7bb
mglgnn <g.migliorelli@gmail.com>
2020-05-27 03:52:38 +0200

    wrapper to testing

---
+++ commit.cc
@@ -0,0 +1,12 @@
+# MSA : phylodist
+The code is under construction. Currently, it allows to read a tree in Newick format from a string and extract the phylogenetic tree it defines. The distance is then computed between any desired pairs of species on the base of branch distances provided as input. 
+
+Todo: 
+
+group functions relating tree construction, visiting, etc into a class 
+
+allow to extract subtrees and rescale distances accordingly 
+
+Recent changes...
+
+Oldest changes...
